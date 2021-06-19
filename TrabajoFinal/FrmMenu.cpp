fcb42c4cd086b4ca4534472ca93fff5ed9c81d62
Salo <salozm34@gmail.com>
2021-06-18 21:37:53 -0500

    Correccion

--- TrabajoFinal/FrmMenu.cpp
+++ TrabajoFinal/FrmMenu.cpp
@@ -1,9 +1,1 @@
 #include "FrmMenu.h"
-
-using namespace TrabajoFinal;
-
-int main()
-{
-	Application::Run(gcnew FrmMenu);
-	return 0;
-}
--- TrabajoFinal/FrmPrincipal.cpp
+++ TrabajoFinal/FrmPrincipal.cpp
@@ -1,2 +1,10 @@
 #include "FrmPrincipal.h"
 
+using namespace TrabajoFinal;
+
+int main()
+{
+	Application::Run(gcnew FrmPrincipal);
+	return 0;
+}
+
